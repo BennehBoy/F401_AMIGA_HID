09508c476330bf8eb70ed97e22d98bcc4b9c9f7b
BennehBoy <bennyboy@benneh.net>
2021-04-20 11:01:28 +0100

    remove debugging comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -154,8 +154,8 @@ void MiddleButtonDown() {
 
 void delay_us (uint16_t us)
 {
-//	__HAL_TIM_SET_COUNTER(&htim4,0);  // set the counter value a 0
-//	while ((uint16_t)__HAL_TIM_GET_COUNTER(&htim4) < us);  // wait for the counter to reach the us input in the parameter
+	__HAL_TIM_SET_COUNTER(&htim4,0);  // set the counter value a 0
+	while ((uint16_t)__HAL_TIM_GET_COUNTER(&htim4) < us);  // wait for the counter to reach the us input in the parameter
 }
 
 
